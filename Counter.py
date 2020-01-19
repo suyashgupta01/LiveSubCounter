c4d6a5968ca1d0a6b5d39e6ed59daf1be070a07f
suyash gupta <suyashgupta0001@gmail.com>
2020-01-19 16:40:42 +0530

    first commit

---
+++ Counter.py
@@ -0,0 +1,14 @@
+import requests as rq
+import json
+import time
+import os
+
+API_KEY = 'AIzaSyDK1l9CuGifoeliJOj2UiyhMhIUKueO0Lw' # enter your own api key
+CHANNEL_ID = 'UCt7iVnJwjBsof8IPLJHCTgQ' # enter the channel id for which subscribers are to be monitored
+
+print("Live Subscriber Count: ")
+
+while True:
+    response = rq.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=' + CHANNEL_ID + '&key=' + API_KEY)
+    print(response.json()['items'][0]['statistics']['subscriberCount'])
+    time.sleep(1)
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Liver Sub Counter
+
+This python script prints the number of subscribers of a given youtube channel (by channel ID) in real time using Youtube Data API.
